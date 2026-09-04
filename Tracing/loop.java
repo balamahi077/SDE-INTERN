aa777127e8908a4a6fae8f42ac6994fdefa08682
balamahi077 <bk6417182@gmail.com>
2026-09-04 10:40:19 +0530

    loops completed

--- Tracing/loop.java
+++ Tracing/loop.java
@@ -3,10 +3,66 @@ class loop {
 
     public static void main(String[] args) {
 
-        for (int i = 1; i <= 20; i++) {
-            if (i % 3 == 0) {
-                System.out.println(i);
-            }
-        }
+    //    for(int index = 1; index <= 5;) {
+    //     System.out.println("Algo ");
+    //    }
+
+    // for(;;) { //infinite
+    //     System.out.println("alogo");
+    // }
+
+    //step forword by 2
+    for(int index = 1; index <= 10; index+=2) { // odd
+        System.out.println(index);
+
     }
+
+    //step forword by 2
+    for(int index = 2; index <= 10; index+=2) { //even
+        System.out.println(index);
+    }
+
+    //step backword by 1
+    for(int index = 10; index >= 1; index--) {
+        System.out.println(index);
+    }
+
+    //step backword by 2
+    for(int index = 10; index >= 1; index-=2) {
+        System.out.println(index);
+    }
+
+    //other valid forms
+    // 1.start from 0
+    for(int index = 0; index<=5; index++) {
+        System.out.println(index);
+    }
+
+    // 2.start from nagative number
+    for(int index = -5; index<=5; index++) {
+        System.out.println(index);
+    }
+
+    // 3.print character
+    for(char index = 'A'; index <= 'Z'; index++) {
+        System.out.println(index);
+    }
+
+    // 4.floating point
+    for(double index = 1.1; index<=5.5; index++) {
+        System.out.println(index);
+    }
+
+    // 5.infinite loop
+    for(int index = 1; index<=5;) {
+        System.out.println(index);
+    }
+
+
+    //Beginner mistakes
+    //1. for(int i=5; i>=1; i++);
+
+    
+
+}
 }
