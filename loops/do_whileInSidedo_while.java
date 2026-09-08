25dd588f464fca66544dbb751036ff874cfe4892
balamahi077 <bk6417182@gmail.com>
2026-09-08 11:38:39 +0530

    do-while loop

---
+++ loops/do_whileInSidedo_while.java
@@ -0,0 +1,20 @@
+package loops;
+
+public class do_whileInSidedo_while {
+    public static void main(String[] args) {
+
+        int index = 1;
+        do
+        {
+            int java = 1;
+            do{
+                System.out.print("* ");
+                java++;
+            }while(java <= 4);
+
+            System.out.println();
+            index++;
+        }while(index <= 3);
+
+    }
+}
