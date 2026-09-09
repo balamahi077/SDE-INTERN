73e637db4e8fef143611106becdb569fc28143fc
balamahi077 <bk6417182@gmail.com>
2026-09-09 14:28:36 +0530

    comment

--- Methods/InstanceMethod.java
+++ Methods/InstanceMethod.java
@@ -2,14 +2,14 @@ package Methods;
 
 public class InstanceMethod {
 
-    void display() {
-        System.out.println("Hello World");
+    void display() {                            // it is non-static method if you want to access this method
+        System.out.println("Hello World");    // must and should be create object
     }
     
     public static void main(String[] args) {
-        
-        InstanceMethod im = new InstanceMethod();
-        im.display();
+
+        InstanceMethod im = new InstanceMethod();  // here object is created 
+        im.display();                               // using object reference i am accessing method
 
     }
 }
