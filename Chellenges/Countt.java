83750a764f99dd1aa59052561b0136d7e62426a9
balamahi077 <bk6417182@gmail.com>
2026-09-11 11:54:31 +0530

    methods

---
+++ Chellenges/Countt.java
@@ -0,0 +1,15 @@
+package Chellenges;
+
+public class Countt {
+    
+    public static void main(String[] args) {
+        int number = 25416;
+        int count = 0;
+        
+        while(number > 0)
+        {
+            number = number/10;
+            System.out.println(number);
+        }
+    }
+}
---
+++ Methods/MethCallMethod.java
@@ -0,0 +1,13 @@
+package Methods;
+
+public class MethCallMethod {
+
+    static void salary() {
+        System.out.println("Salary Generated");
+    }
+    
+    public static void main(String[] args) 
+    {
+        salary();
+    }
+}
